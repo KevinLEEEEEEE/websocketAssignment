c04841d9268bdc4779cc97e0843edc7a93556086
Kevin <1069440019@qq.com>
2018-05-06 16:00:41 +0800

    final update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const WebSocket = require('ws');
-
+ 
 const wss = new WebSocket.Server({ port: 8080 });
-
+ 
+// Broadcast to all.
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
@@ -9,12 +10,12 @@ wss.broadcast = function broadcast(data) {
     }
   });
 };
-
+ 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
